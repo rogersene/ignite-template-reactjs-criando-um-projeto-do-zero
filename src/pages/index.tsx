e67053a02882bd3463859781afa0edde9a0f2d9f
rogersene <rogersenefaria@gmail.com>
2022-01-28 20:12:43 -0300

    Home screen implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next';
-
 import { getPrismicClient } from '../services/prismic';
+import Head from 'next/head'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,9 +24,23 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+ export default function Home() {
+   return (
+    <>
+    <Head>
+      <title>Home | Blog Roger </title>
+    </Head>
+
+    <main className={styles.contentContainer}>
+      <img src="/Logo.svg" alt="logo"/>
+      <div className={styles.containerPost}>
+        <h1>Como utilizar Hooks</h1>
+        <p>Pensando em sincronização em vez de ciclos de vida.</p>
+      </div>
+    </main>
+    </>
+   )
+ }
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
